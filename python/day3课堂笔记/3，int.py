fec030c2242f845d98b782e1b41abee2a78d3616
wldu <frank.du2015@gmail.com>
2019-09-15 23:41:55 -0400

    Add files via upload

---
+++ python/day3课堂笔记/3，int.py
@@ -0,0 +1,13 @@
+i = 100
+print(i.bit_length())
+'''
+                  bit_length
+1     0000 0001       1
+2     0000 0010       2
+3     0000 0011       2
+'''
+
+
+i = 5
+print(i.to_bytes(3, 'big'))
+print(bool(' '))
